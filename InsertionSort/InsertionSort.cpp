ca22e24c1ad75f401f8d2f74cf57df95b091c448
nauvalachid <nauvalachid@gmail.com>
2023-03-23 15:18:48 +0700

    Procedure Insertionsort

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -32,3 +32,34 @@ void input()
 		cin >> arr[i];		// Memasukkan nilai data n kedalam Array arr
 	}
 };
+// Procedure Insertionsort
+void insertionsort()
+{
+
+	int temp; // Membuat variabel data temporer atau penyimpanan sementara
+	int j; // Membuat variabel j sebagai penanda
+
+	for (int i = 1; i < n; i++) // 1.Looping dengan i dimulai dari 1 hingga n-1
+	{
+
+		temp = arr[i];	// 2. Simpan nilai arr[i] ke variabel sementara temp
+
+		j = i - 1;	// 3. Setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp) // 4. Looping while dimana nilai j lebih besar
+		{								// arr[j] lebih besar daripada temp
+			arr[j + 1] = arr[j];	// a. simpan arr[j] ke dalam variabel arr[j + 1]
+			j--;					// b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;		// 5. simpan nilai temp ke dalam arr[j+1]
+
+
+		cout << endl;
+		cout << "\nPass " << i << ": "; // Output ke layar
+		for (int k = 0; k < n; k++)		// Looping nilai k dimulai dari 0 hingga 
+		{
+			cout << arr[k] << " ";		// Output ke layar
+		}
+	}
+};
